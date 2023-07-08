447f1ae34c156bb5ce33170dffd523436f242f96
LengGeng <906506482@qq.com>
2023-07-08 22:54:41 +0800

    feat: 修改网站标题和 Logo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   description: 'VitePress 项目模板，包含一些基础配置。你可以通过它快速创建一个 VitePress 项目。',
   lang: 'cn-ZH',
   base: '/',
-  lastUpdated: true
+  lastUpdated: true,
+  themeConfig: {
+    logo: '/logo.svg',
+    siteTitle: 'WebSite Title'
+  }
 })
