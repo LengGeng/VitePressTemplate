7c1d268e0a90bed673e6645f0ce7da666e98028f
LengGeng <906506482@qq.com>
2023-07-08 16:06:27 +0800

    Initial

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress Template',
+  description: 'VitePress 项目模板，包含一些基础配置。你可以通过它快速创建一个 VitePress 项目。',
+  lang: 'cn-ZH',
+  base: '/',
+  lastUpdated: true
+})
